9dafef51e442d2acbbb4e5e2ec7924fce8442797
joshuapras <97777283+joshuapras@users.noreply.github.com>
2022-02-04 03:48:58 +0700

    menyelesaikan video class dan object

---
+++ pertemuan2/Coba.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Joshua Praisyatama
+193040189
+https://github.com/joshuapras/prakweb2021_oophp_193040189
+Pertemuan 2 -  Januari 2022
+Membuat Class dan Object
+*/
+?>
+<?php
+
+class Coba {
+
+
+}
+
+
+$a = new Coba();
+$b = new Coba();
+$c = new Coba();
+
+var_dump($a);
+var_dump($b);
+var_dump($c);
+
+?>
---
+++ pertemuan2/index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Joshua Praisyatama
+193040189
+https://github.com/joshuapras/prakweb2021_oophp_193040189
+Pertemuan 2 -  Januari 2022
+Membuat Class dan Object studi kasus
+*/
+?>
+<?php 
+
+class Produk {
+ 
+}
+
+$produk1 = new Produk();
+$produk2 = new Produk();
+
+var_dump($produk1);
+
+?>
